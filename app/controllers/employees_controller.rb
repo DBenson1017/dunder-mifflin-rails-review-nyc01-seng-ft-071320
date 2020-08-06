9597bdc3e03f83922a6b6fba522883e8f04d8b43
DBenson1017 <devinbenson@gmail.com>
2020-08-06 16:32:02 -0400

    deliverables up to/not including CRUD actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,10 @@
 class EmployeesController < ApplicationController
+    def index 
+        @employees = Employee.all 
+    end 
+     
+    def show
+        @emp = Employee.find(params[:id])
+    end 
+
 end
