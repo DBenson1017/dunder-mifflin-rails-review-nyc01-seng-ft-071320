e6703e460b29947143ba90d8a6adcdc9e3753337
DBenson1017 <devinbenson@gmail.com>
2020-08-06 18:11:24 -0400

    beginning of emp new form

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,4 +7,25 @@ class EmployeesController < ApplicationController
         @emp = Employee.find(params[:id])
     end 
 
+    def new 
+        @employee = Employee.create(employee_params)
+    end 
+
+    def edit
+        @employee = Employee.find(params[:id]) 
+    end 
+
+    def update 
+        @employee = Employee.find(params[:id])
+        @employee.update(emloyee_params)
+        redirect_to employee_path(@employee)
+
+    end 
+
+
+
+
+
+
+
 end
